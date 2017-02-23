26458083daa24f53d42c5e51dda0f43001fa5a37
PETER <polygamers@hotmail.com>
2017-02-23 17:10:49 +0100

    Update main.py
    Ajout de l adapter list

--- main.py
+++ main.py
@@ -81,6 +81,43 @@ class AndroidBuild(CrossBuild):
                 }
         }"""
         return ret
+    
+    def buildObjectListAdapter(self, model):
+        ret="""public class """+model["model"]["name"]+"""Adapter extends ArrayAdapter<"""+model["model"]["name"]+"""> {
+
+        private static class ViewHolder {
+            private TextView itemView;
+        }
+
+        public """+model["model"]["name"]+"""Adapter(Context context, int textViewResourceId, ArrayList<"""+model["model"]["name"]+"""> items) {
+            super(context, textViewResourceId, items);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(this.getContext())
+                .inflate(R.layout.listview_association, parent, false);
+
+                viewHolder = new ViewHolder();
+                viewHolder.itemView = (TextView) convertView.findViewById(R.id.ItemView);
+
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            MyClass item = getItem(position);
+            if (item!= null) {
+                // My layout has only one TextView
+                    // do whatever you want with your string and long
+                viewHolder.itemView.setText(String.format("%s %d", item.reason, item.long_val));
+            }
+
+            return convertView;
+        }
+        } """
+        return ret
 
     def buildObjectListFragment(self, model):
         ret = """
