300573fcc2cc107f08036d3abeb39455086d1c12
PETER <polygamers@hotmail.com>
2017-02-24 11:19:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,12 +42,12 @@ class AndroidBuild(CrossBuild):
         self.packageName = packageName
 
     def buildDetailView(self, model, view):
-        self.output.append("java", "path/file.ext", self.buildObjectDetailFragment(model, view))
-        self.output.append("xml", "path/file.ext", self.buildLayoutDetail(model, view))
+        self.output.append(["java", "path/file.ext", self.buildObjectDetailFragment(model, view)])
+        self.output.append(["xml", "path/file.ext", self.buildLayoutDetail(model, view)])
 
     def buildListView(self, model, view):
-        self.output.append("java", "path/file.ext", self.buildObjectListFragment(model, view))
-        self.output.append("java", "path/file.ext", self.buildObjectListAdapter(model, view))
+        self.output.append(["java", "path/file.ext", self.buildObjectListFragment(model, view)])
+        self.output.append(["java", "path/file.ext", self.buildObjectListAdapter(model, view)])
 
     def buildObjectDetailFragment(self, model, view):
         ret="""
