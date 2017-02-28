46c5ca3e4b912ed03c9a550dafedd7dc0667f582
PETER <polygamers@hotmail.com>
2017-02-28 16:55:33 +0100

    Update main.py
    Ajout de django model

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class CrossBuild():
         with open(config) as data_file:
             self.config = json.load(data_file)
 
+    def first(self, iterable, default=None):
+        for item in iterable:
+            return item
+        return default
+
     def build(self):
         for build in self.config:
             # models
@@ -21,7 +26,7 @@ class CrossBuild():
                 if(view["type"] == "listView"):
                     self.buildListView(build["models"], view)
                 if(view["type"] == "detailView"):
-                    self.buildDetailView(build["models"], view)
+                    self.buildDetailView(self.first(x for x in build["models"] if x["name"] == view["model"]), view)
 
     def buildModel(self, model):
         pass
@@ -45,27 +50,86 @@ class CrossBuild():
         return ret
 
 class DjangoBuild(CrossBuild):
-    def build(self):
-        pass
+    path = "./django/"
 
-    def buildModel(self):
-        pass
+    def buildModel(self, model):
+        self.output.append(("python", self.path+"models.py", self.buildModelFile(model)))
 
+    def buildModelFile(self, model):
+        ret = """
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Categorie (models.Model):"""
+        for field in model["attributes"]:
+            if field["type"] == "Char":
+                ret += """
+    """+field["name"]+""" = models."""+field["type"]+"""Field(max_length="""+field["options"]["max_length"]+""")"""
+
+            if field["type"] == "Boolean":
+                ret += """
+    """+field["name"]+""" = models."""+field["type"]+"""Field(default=False)"""
+
+        ret+= """
+
+    def __str__(self):
+        """
+        ret += 'return ' + ', '.join([str(field) for field in model["display"]])
+
+        return ret
 
 class AndroidBuild(CrossBuild):
-    ctype = {"Auto": "Integer", "Char": "String", "Boolean": "boolean"}
+    ctype = {"Auto": "Integer", "BigAuto": "Long", "BigInteger": "Long", "Char": "String", "Boolean": "boolean", "Integer": "Integer"}
 
     def __init__(self, packageName):
         self.packageName = packageName
 
     def buildDetailView(self, model, view):
-        pass
+        self.output.append(("xml", "./android/res/"+model["name"]+".xml", self.buildUpdateLayout(model)))
 
     def buildListView(self, model, view):
         pass
 
     def buildModel(self, model):
-        self.output.append(("java", "./android/model/"+inflection.underscore(model["name"]), self.buidlModelEntity(model)))
+        self.output.append(("java", "./android/model/"+model["name"]+".java", self.buidlModelEntity(model)))
+
+    def buildUpdateLayout(self, model):
+        ret = """
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+    """
+        for field in model["attributes"]:
+            if field["type"] == "Char":
+                ret+="""
+    <EditText
+        android:id="@+id/"""+field["name"]+"""\"
+        android:hint="@string/"""+field["label"]+"""\"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:singleLine="true"/>"""
+
+            if field["type"] == "Boolean":
+                ret+="""
+    <CheckBox
+        android:id="@+id/"""+field["name"]+"""\"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="@string/"""+field["label"]+"""\" />"""
+
+        ret+="""
+
+    <Button
+        android:layout_width="fill_parent"
+        android:layout_height="wrap_content"
+        android:id="@+id/update"""+model["name"]+"""\"
+        android:text="Modifier" />
+
+</RelativeLayout>
+        """
+        return ret
 
     def buidlModelEntity(self, model):
         ret="""package com.efenua.courselocal.model;
@@ -168,7 +232,7 @@ public class """ + model["name"] + """ implements Parcelable {"""
 
 
 if __name__ == "__main__":
-    course = AndroidBuild("fr.efenua.course")
+    course = DjangoBuild()
     course.loadConfig("../crossCodeData/config.json")
     course.build()
     print(course.render())
