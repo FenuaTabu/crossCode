796e9288f3240c9763b4e308e37ad4e0ef4d1ce9
PETER <polygamers@hotmail.com>
2017-03-02 23:11:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ class CrossBuild():
     config = None
     output = []
 
-    def loadConfig(self, config):
+    def load_config(self, config):
         self.config = config
 
     def build(self):
+        '''Construction des models et vues'''
+        # models
         for model in self.config.models:
-            # models
-            self.buildModel(model)
+            self.build_model(model)
 
-            # views
+        # views
         for view in self.config.views:
             if(view.type == "listView"):
-                self.buildListView(build.models, view)
+                self.build_listview(next(x for x in self.config.models if x.name==view.model), view)
             if(view.type == "detailView"):
-                self.buildDetailView(build.models, view)
+                self.build_detailview(next(x for x in self.config.models if x.name==view.model), view)
 
-    def buildModel(self, model):
+    def build_model(self, model):
         pass
 
-    def buildDetailView(self, model, view):
+    def build_detailview(self, model, view):
         pass
 
-    def buildListView(self, model, view):
+    def build_listview(self, model, view):
         pass
 
     def render(self):
@@ -44,10 +45,10 @@ class CrossBuild():
 class DjangoBuild(CrossBuild):
     path = "./django/"
 
-    def buildModel(self, model):
-        self.output.append(("python", self.path+"models.py", self.buildModelFile(model)))
+    def build_model(self, model):
+        self.output.append(("python", self.path+"models.py", self.build_model_file(model)))
 
-    def buildModelFile(self, model):
+    def build_model_file(self, model):
         ret = """
 from __future__ import unicode_literals
 
@@ -72,21 +73,137 @@ class Categorie (models.Model):"""
         return ret
 
 class AndroidBuild(CrossBuild):
-    ctype = {"Auto": "Integer", "BigAuto": "Long", "BigInteger": "Long", "Char": "String", "Boolean": "boolean", "Integer": "Integer"}
+    ctype = {"Auto": "Integer", "BigAuto": "Long", "BigInteger": "Long", "Char": "String", "Boolean": "boolean", "Integer": "Integer", "ForeignKey": "ForeignKey"}
 
     def __init__(self, packageName):
         self.packageName = packageName
 
-    def buildDetailView(self, model, view):
-        self.output.append(("xml", "./android/res/"+model.name+".xml", self.buildUpdateLayout(model)))
+    def build_detailview(self, model, view):
+        self.output.append(("xml", "./android/res/"+model.name+".xml", self.build_update_layout(model)))
 
-    def buildListView(self, model, view):
-        pass
+    def build_listview(self, model, view):
+        self.output.append(("java", "./android/activity/"+model.name+"ListActivity.java", self.build_listview_activity(model, view)))
+        self.output.append(("java", "./android/adapter/"+model.name+"Adapter.java", self.build_listview_adapter(model, view)))
 
-    def buildModel(self, model):
-        self.output.append(("java", "./android/model/"+model.name+".java", self.buidlModelEntity(model)))
+    def build_model(self, model):
+        self.output.append(("java", "./android/model/"+model.name+".java", self.build_model_entity(model)))
 
-    def buildUpdateLayout(self, model):
+    def build_listview_adapter(self, model, view):
+        ret = '''
+package com.efenua.courselocal.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.efenua.courselocal.R;
+import com.efenua.courselocal.model.'''+model.name+''';
+
+import java.util.ArrayList;
+
+/**
+ * Generate by crossCode.
+ */
+
+public class '''+model.name+'''Adapter extends ArrayAdapter<'''+model.name+'''> {
+    public '''+model.name+'''Adapter(Context context, ArrayList<'''+model.name+'''> list'''+model.name+''') {
+        super(context, 0, list'''+model.name+''');
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        '''+model.name+''' obj = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate (R.layout.item_'''+inflection.underscore(model.name)+''', parent, false);
+        }
+
+        TextView textViewLib = (TextView) convertView.findViewById(R.id.textViewLib);
+        textViewLib.setText(obj.getLib());
+
+        return convertView;
+    }
+}
+
+        '''
+        return ret
+        
+    def build_listview_activity(self, model, view):
+        ret = '''
+package com.efenua.courselocal.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.efenua.courselocal.R;
+import com.efenua.courselocal.adapter.'''+model.name+'''Adapter;
+import com.efenua.courselocal.model.'''+model.name+''';
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class '''+model.name+'''ListActivity extends AppCompatActivity {
+    private static final String EXTRA_ADD_UPDATE = "com.efenua.courselocal.add_update";
+    private ListView listView;
+    private ArrayList<'''+model.name+'''> al'''+model.name+''';
+    private '''+model.name+'''Adapter '''+inflection.camelize(model.name, False)+'''Adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_'''+inflection.underscore(model.name)+'''_list);
+
+
+        listView = (ListView) findViewById(R.id.listView'''+model.name+''');
+        reloadData();
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+                Intent i = new Intent('''+model.name+'''ListActivity.this, '''+model.name+'''DetailActivity.class);
+                i.putExtra(EXTRA_ADD_UPDATE, "Update");
+                i.putExtra("'''+inflection.underscore(model.name)+'''", ('''+model.name+''') al'''+model.name+'''.get(pos));
+                startActivity(i);
+                return true;
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent('''+model.name+'''ListActivity.this, '''+model.name+'''DetailActivity.class);
+                i.putExtra(EXTRA_ADD_UPDATE, "Add");
+                startActivity(i);
+            }
+        });
+    }
+
+    public void reloadData() {
+        al'''+model.name+''' = new ArrayList<'''+model.name+'''>('''+model.name+'''.listAll('''+model.name+'''.class)) ;
+
+        '''+inflection.camelize(model.name, False)+'''Adapter = new '''+model.name+'''Adapter(this, al'''+model.name+''');
+        listView.setAdapter('''+inflection.camelize(model.name, False)+'''Adapter);
+    }
+
+}
+        '''
+        return ret
+        
+    def build_update_layout(self, model):
         ret = """
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:tools="http://schemas.android.com/tools"
@@ -123,20 +240,26 @@ class AndroidBuild(CrossBuild):
         """
         return ret
 
-    def buidlModelEntity(self, model):
+    def build_model_entity(self, model):
         ret="""package com.efenua.courselocal.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.orm.SugarRecord;
+
 /**
  * Generate by crossCode.
  */
 
-public class """ + model.name + """ implements Parcelable {"""
+public class """ + model.name + """  extends SugarRecord implements Parcelable {"""
 
         for field in model.attributes:
-            ret += """
+            if field.type == "ForeignKey":
+                ret += """
+    private """ + field.options["othermodel"] + """ """ + field.name + """;"""
+            else:
+                ret += """
     private """ + self.ctype.get(field.type) + """ """ + field.name + """;"""
 
         ret+="""
@@ -145,7 +268,7 @@ public class """ + model.name + """ implements Parcelable {"""
     }
 
     public """ + model.name + """("""
-        ret +=', '.join("??")
+        ret +=', '.join(self.ctype.get(x.type) + " " + x.name for x in model.attributes)
         ret +=  """) {"""
 
         for field in model.attributes:
@@ -160,6 +283,9 @@ public class """ + model.name + """ implements Parcelable {"""
             if(field.type == "Boolean"):
                 ret += """
         """ + field.name + """ = in.readByte() != 0;"""
+            if(field.type == "ForeignKey"):
+                ret += """
+        """ + field.name + """ = (""" + field.options["othermodel"] + """) in.readParcelable(""" + field.options["othermodel"] + """.class.getClassLoader()); """
             else:
                 ret += """
         """ + field.name + """ = in.read""" + self.ctype.get(field.type) + """(); """
@@ -181,16 +307,30 @@ public class """ + model.name + """ implements Parcelable {"""
 
     """
         for field in model.attributes:
-            ret += """
-    public """ + self.ctype.get(field.type) + """ get""" + field.name + """() {
+            if(field.type == "ForeignKey"):
+                ret += """
+    public """ + field.options["othermodel"] + """ get""" + inflection.camelize(field.name) + """() {
+        return """ + field.name + """;
+    }
+    """
+            else:
+                ret += """
+    public """ + self.ctype.get(field.type) + """ get""" + inflection.camelize(field.name) + """() {
         return """ + field.name + """;
     }
     """
 
         for field in model.attributes:
-            ret += """
-    public void set""" + field.name + """(""" + self.ctype.get(field.type) + """ """ + field.name + """) {
-        return """ + field.name + """;
+            if(field.type == "ForeignKey"):
+                ret += """
+    public void set""" + inflection.camelize(field.name) + """(""" + field.options["othermodel"] + """ """ + field.name + """) {
+        this.""" + field.name + """ = """ + field.name + """;
+    }
+    """
+            else:
+                ret += """
+    public void set""" + inflection.camelize(field.name) + """(""" + self.ctype.get(field.type) + """ """ + field.name + """) {
+        this.""" + field.name + """ = """ + field.name + """;
     }
     """
 
@@ -204,14 +344,16 @@ public class """ + model.name + """ implements Parcelable {"""
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         """
-
         for field in model.attributes:
             if(field.type == "Boolean"):
                 ret += """
         dest.writeByte((byte) ("""+field.name+""" ? 1 : 0)); """
+            if(field.type == "ForeignKey"):
+                ret += """
+        dest.writeParcelable("""+field.name+""", flags);"""
             else:
                 ret += """
-        dest.write"""+self.ctype.get(field.type)+"""(field.name)"""
+        dest.write"""+self.ctype.get(field.type)+"""("""+field.name+""");"""
         ret += """
     }
 
@@ -222,50 +364,72 @@ public class """ + model.name + """ implements Parcelable {"""
 """
         return ret
         
-class Attribute():
-    options = {}
-    def __init__(self, name, type):
+class Attribute(object):
+    def __init__(self, name, type, options=None):
         self.name = name
         self.type = type
+        self.options = {}
+        if options is not None:
+            for opt_key, opt_value in options.items():
+                self.add_option(opt_key, opt_value)
         
-    def addOption(self, key, value):
+    def add_option(self, key, value):
         self.options[key]= value
 
-class Model():
-    attributes = []
-    
+class Model(object):
     def __init__(self, name):
         self.name = name
+        self.attributes = []
       
-    def addAttribute(self, attribute):
-        self.attributes.append(attribute)
-
-class Config():
-    models = []
-    views = []
-    
-    def __init__(self):
-        pass
+    def add_attribute(self, attrs_name, attrs_type, options=None):
+        new_attrs = Attribute(attrs_name, attrs_type, options)
+        self.attributes.append(new_attrs)
         
-    def addModel(self, model):
+class View(object):
+    def __init__(self, type, model):
+        self.type = type
+        self.model = model
+        self.fields = []
+        
+    def add_field(self, field):
+        self.fields.append(field)
+
+class Config(object):
+    def __init__(self):
+        self.models = []
+        self.views = []
+        
+    def add_model(self, model):
         self.models.append(model)
+        
+    def add_view(self, view):
+        self.views.append(view)
 
 
 config = Config()
 
+# Course
 course = Model("Course")
 course.display = "lib"
-course.addAttribute(Attribute("id", "BigAuto"))
-attrs = Attribute("lib", "Char")
-attrs.addOption("max_length", "40")
-course.addAttribute(attrs)
-course.addAttribute(Attribute("panier", "Boolean"))
+course.add_attribute("id", "BigAuto")
+course.add_attribute("lib", "Char", {"max_length": "40"})
+course.add_attribute("panier", "Boolean")
 
-config.addModel(course)
+# Article
+article = Model("Article")
+article.display = "lib"
+article.add_attribute("id", "BigAuto")
+article.add_attribute("lib", "Char", {"max_length": "40"})
+article.add_attribute("categorie", "ForeignKey", {"othermodel": "Categorie"})
 
+list_view_course = View("listView", "Article")
+
+config.add_model(course)
+config.add_model(article)
+config.add_view(list_view_course)
 
 if __name__ == "__main__":
     course = AndroidBuild("")
-    course.loadConfig(config)
+    course.load_config(config)
     course.build()
     print(course.render())
